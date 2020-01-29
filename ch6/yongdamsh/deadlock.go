6bc9f5f798223b540619c5e39901e5c06f8c7ef4
Sanghyeon Lee <yongdamsh@gmail.com>
2020-01-29 23:22:26 +0900

    Add notes for chapter 6

---
+++ ch6/yongdamsh/deadlock.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+  cnt := int64(10)
+
+  for i := 0; i < 10; i++ {
+    go func() {
+      cnt--
+    }()
+  }
+
+  if cnt > 0 {
+    time.Sleep(100 * time.Millisecond)
+  }
+
+  fmt.Println(cnt)
+}
